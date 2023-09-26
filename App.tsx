4393b81620d66477da791f75e2c7903e2a3d9b2f
Soonho Kwon <percy3368@gmail.com>
2023-09-26 13:38:32 +0900

    :sparkles: Impl barcode scanning example

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+import { Camera, CameraType } from "expo-camera";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default function App () {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    return <Text>Waiting for camera permission...</Text>;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Camera
+        style={styles.camera}
+        type={CameraType.back}
+        barCodeScannerSettings={{
+          barCodeTypes: [
+            BarCodeScanner.Constants.BarCodeType.ean8,
+            BarCodeScanner.Constants.BarCodeType.ean13,
+          ]
+        }}
+        onBarCodeScanned={res => {
+          console.log(res);
+        }}
+      >
+      </Camera>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
