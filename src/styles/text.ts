17c121299048fe1cbfcf141268e5e9a0defc4c68
yuwol <yuwol036@gmail.com>
2023-10-02 08:02:47 +0900

    :lipstick: Add emphasized typographies

--- src/styles/text.ts
+++ src/styles/text.ts
@@ -3,10 +3,12 @@ import { mapColors, type ColorKeys } from "./color";
 
 type Typography =
   | "title1"
+  | "title1Emph"
   | "title2"
   | "title3"
   | "headline"
   | "body"
+  | "bodyEmph"
   | "callout"
   | "subhead"
   | "footnote"
@@ -21,10 +23,12 @@ type Typography =
  */
 export const text: Record<Typography | ColorKeys, TextStyle> = {
   title1: { fontSize: 28, lineHeight: 34 },
+  title1Emph: { fontSize: 28, lineHeight: 34, fontWeight: "700" },
   title2: { fontSize: 22, lineHeight: 28 },
   title3: { fontSize: 20, lineHeight: 25 },
   headline: { fontSize: 17, lineHeight: 22, fontWeight: "600" },
   body: { fontSize: 17, lineHeight: 22 },
+  bodyEmph: { fontSize: 17, lineHeight: 22, fontWeight: "600" },
   callout: { fontSize: 16, lineHeight: 21 },
   subhead: { fontSize: 15, lineHeight: 20 },
   footnote: { fontSize: 13, lineHeight: 18 },
