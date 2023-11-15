6ab1cab8ea72fdcefcdfb09eedba54fb21f1132a
yuwol <yuwol036@gmail.com>
2023-11-15 17:14:06 +0900

    :sparkles: Apply font family Pretendard

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
-import { RootStack } from "@/navigation/RootStack";
+import React, { useCallback } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import { RootStack } from "@/navigation/RootStack";
 
 // Temp fix to suppress Animated warning
 import { Animated } from "react-native";
@@ -7,10 +11,27 @@ import { Animated } from "react-native";
 const av = new Animated.Value(0);
 av.addListener(() => {});
 
-const App = () => (
-  <SafeAreaProvider>
-    <RootStack />
-  </SafeAreaProvider>
-);
+SplashScreen.preventAutoHideAsync();
+
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    "Pretendard-Regular": require("./assets/fonts/Pretendard-1.3.9/Pretendard-Regular.otf"),
+    "Pretendard-Medium": require("./assets/fonts/Pretendard-1.3.9/Pretendard-Medium.otf"),
+    "Pretendard-SemiBold": require("./assets/fonts/Pretendard-1.3.9/Pretendard-SemiBold.otf"),
+    "Pretendard-Bold": require("./assets/fonts/Pretendard-1.3.9/Pretendard-Bold.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <RootStack />
+    </SafeAreaProvider>
+  );
+};
 
 export default App;
