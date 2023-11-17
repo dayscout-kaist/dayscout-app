3ebec036034b1ce453ca3e63c548dd0b36b0dd18
wjeongchoi <artificialmint@gmail.com>
2023-11-17 12:09:00 +0900

    Fix design

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -17,6 +17,8 @@ export const Settings: React.FC = () => {
   const [weight, setWeight] = useState('');
   const avatarSrc = "https://sparcs-newara-dev.s3.amazonaws.com/files/NewAra_Channeltalk.jpg"
   const nickname = "나도이제개발"
+
+
   return (
     <View style={[fill]}>
       <View style={[bg.white,padding.horizontal(safe.horizontal)]}>
@@ -24,7 +26,7 @@ export const Settings: React.FC = () => {
       </View>
       <ScrollView>
         <View style={[column, gap(12)]}>
-          <View style={[bg.white,padding.horizontal(safe.horizontal),padding.vertical(16)]}>
+          <View style={[bg.white,padding.horizontal(safe.horizontal),padding.vertical(32)]}>
               <Text style={[text.h3, margin.bottom(16)]}>계정</Text>
               <View style ={[row, justify.start, align.center]}>
                 <Image
@@ -39,34 +41,42 @@ export const Settings: React.FC = () => {
 
           </View>
 
-          <View style={[bg.white,padding.horizontal(safe.horizontal),padding.vertical(16)]}>
+          <View style={[bg.white,padding.horizontal(safe.horizontal),padding.vertical(32)]}>
             <Text style={[text.h3, margin.bottom(16)]}>신체 정보 수정</Text>
             <View style={[row, justify.between, align.center]}>
               <Text style={[text.sub2,margin.right(16)]}>키</Text>
-              <TextInput
-                style={[text.body1, {width:30,textAlign: 'right'}]} 
-                onChangeText={setHeight}
-                value={height}
-                keyboardType="numeric"
-                returnKeyType="done" 
-                onSubmitEditing={Keyboard.dismiss}
-              />
-              <Text style={[text.body1, text.gray500,margin.right(16)]}>cm</Text>
+              <View style={[row, round.lg, bg.gray100, justify.between, align.center]}>
+                <TextInput
+                  style={[text.body1, margin.right(8),margin.bottom(4),{width:48,textAlign: 'right'}]} 
+                  onChangeText={setHeight}
+                  value={height}
+                  keyboardType="numeric"
+                  returnKeyType="done" 
+                  placeholder="160"
+                  onSubmitEditing={Keyboard.dismiss}
+                />
+                <Text style={[text.body1, text.gray500, margin.right(8)]}>cm</Text>
+
+              </View>
 
               <Text style={[text.sub2,margin.right(16)]}>체중</Text>
-              <TextInput
-                style={[text.body1, { width:30, textAlign: 'right'}]}
-                onChangeText={setWeight}
-                value={weight}
-                keyboardType="numeric"
-                returnKeyType="done" 
-                onSubmitEditing={Keyboard.dismiss}
-              />
-              <Text style={[text.body1,text.gray500]}>kg</Text>
+              <View style={[row, round.lg, bg.gray100,justify.between, align.center]}>
+
+                <TextInput
+                  style={[text.body1, margin.right(8),margin.bottom(4),{ width:48, textAlign: 'right'}]}
+                  onChangeText={setWeight}
+                  value={weight}
+                  keyboardType="numeric"
+                  returnKeyType="done" 
+                  placeholder="55"
+                  onSubmitEditing={Keyboard.dismiss}
+                />
+                <Text style={[text.body1,text.gray500,  margin.right(8)]}>kg</Text>
+              </View>
             </View>
           </View>
 
-          <View style={[bg.white,padding.horizontal(safe.horizontal),padding.vertical(16)]}>
+          <View style={[bg.white,padding.horizontal(safe.horizontal),padding.vertical(32)]}>
             <Text style={[text.h3, margin.bottom(8)]}>알람 설정</Text>
             <View style={[row, justify.between, align.center, margin.vertical(8)]}>
               <Text style={[text.body1]}>식후 1시간</Text>
