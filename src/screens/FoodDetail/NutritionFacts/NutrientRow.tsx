e4cd299e06e3796b15fb93d00503516b156abcb8
yuwol <yuwol036@gmail.com>
2023-11-20 14:31:58 +0900

    :truck: Move NutritionFacts to directory

--- src/screens/FoodDetail/NutritionFacts/NutrientRow.tsx
+++ src/screens/FoodDetail/NutritionFacts/NutrientRow.tsx
@@ -3,12 +3,12 @@ import { Text, View } from "react-native";
 
 import { justify, row, text } from "@/styles";
 
-export const NutrientRow: React.FC<{ name: string; value: string }> = ({
-  name,
-  value,
-}) => (
+export const NutrientRow: React.FC<{
+  name: string;
+  value: number | undefined;
+}> = ({ name, value }) => (
   <View style={[row, justify.between]}>
     <Text style={[text.body1, text.gray400]}>{name}</Text>
-    <Text style={[text.body1, text.gray600]}>{value}</Text>
+    <Text style={[text.body1, text.gray600]}>{value ?? "?"}g</Text>
   </View>
 );
