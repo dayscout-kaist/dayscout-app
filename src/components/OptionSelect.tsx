6d7ae43710ea2c682ce259c2bc77662a08e49dad
yuwol <yuwol036@gmail.com>
2023-11-30 01:51:46 +0900

    :sparkles: Impl `OptionSelect` input component

---
+++ src/components/OptionSelect.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { Icon } from "@/icons";
+import {
+  align,
+  bg,
+  colors,
+  fill,
+  gap,
+  margin,
+  padding,
+  round,
+  row,
+  text,
+} from "@/styles";
+
+export const OptionSelect: React.FC<{
+  value: string | undefined;
+  title?: string;
+  placeholder?: string;
+  onPress: () => void;
+}> = ({ value, title, placeholder, onPress }) => {
+  const notSelected = value === undefined;
+
+  return (
+    <View style={gap(6)}>
+      {title && (
+        <Text style={[margin.left(4), text.sub2, text.gray600]}>{title}</Text>
+      )}
+      <TouchableOpacity
+        style={[
+          row,
+          align.center,
+          gap(10),
+          round.md,
+          padding.vertical(18),
+          padding.horizontal(16),
+          bg.gray50,
+        ]}
+        onPress={onPress}
+      >
+        <Text
+          style={[fill, text.btn1, notSelected ? text.gray300 : text.gray600]}
+        >
+          {notSelected && placeholder ? placeholder : value}
+        </Text>
+        <Icon.down_wide width={26} height={26} fill={colors.gray500} />
+      </TouchableOpacity>
+    </View>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from "./Button";
 export * from "./Clickable";
 export * from "./FoodSearchItem";
 export * from "./FormLayout";
+export * from "./OptionSelect";
 export * from "./ScreenBackground";
 export * from "./Tag";
 export * from "./TextInput";
