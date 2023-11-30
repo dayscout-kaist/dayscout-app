feff4c0698667fc61ac48634e06dfe6c8a826630
yuwol <yuwol036@gmail.com>
2023-12-01 04:31:58 +0900

    :sparkles: Add `Notice` component

---
+++ src/components/Notice.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { bg, center, fill, h, justify, margin, text } from "@/styles";
+import { TossFace } from "@/utils/TossFace";
+
+export const Notice: React.FC<{ icon: string; msg: string }> = ({
+  icon,
+  msg,
+}) => (
+  <View style={[fill, center, bg.white]}>
+    <View style={[justify.center, h(128)]}>
+      <TossFace icon={icon} size={64} />
+    </View>
+    <Text style={[text.sub2, text.gray400]}>{msg}</Text>
+  </View>
+);
