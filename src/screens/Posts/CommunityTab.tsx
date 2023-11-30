c45f08434f9421601a737f1ff0d6ae2605b19e31
yuwol <yuwol036@gmail.com>
2023-12-01 03:37:38 +0900

    :adhesive_bandage: Change unsupported emoji

--- src/screens/Posts/CommunityTab.tsx
+++ src/screens/Posts/CommunityTab.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { bg, center, fill, margin, text } from "@/styles";
+import { bg, center, fill, h, justify, margin, text } from "@/styles";
 import { TossFace } from "@/utils/TossFace";
 
 export const CommunityTab: React.FC = () => {
@@ -10,7 +10,9 @@ export const CommunityTab: React.FC = () => {
 
   return (
     <View style={[fill, center, margin.bottom(60 + insets.bottom), bg.white]}>
-      <TossFace icon="🏗️" size={64} />
+      <View style={[justify.center, h(128)]}>
+        <TossFace icon="🚧" size={64} />
+      </View>
       <Text style={[text.sub2, text.gray400]}>준비 중인 기능이에요!</Text>
     </View>
   );
