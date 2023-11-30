23d157fa558b40f4aa06d1c2074a28d31bbdaf40
yuwol <yuwol036@gmail.com>
2023-12-01 00:29:02 +0900

    :sparkles: Set `Clickable` bg color as prop

--- src/components/Clickable.tsx
+++ src/components/Clickable.tsx
@@ -1,23 +1,29 @@
 import React from "react";
-import { Motion } from "@legendapp/motion";
-import { bg, springMotion } from "@/styles";
 import type { StyleProp, ViewStyle } from "react-native";
+
+import { Motion } from "@legendapp/motion";
+
+import { bg, springMotion } from "@/styles";
 
 interface Props extends React.ComponentProps<typeof Motion.Pressable> {
   children: React.ReactNode;
   viewStyle?: StyleProp<ViewStyle>;
+  initialBg?: ViewStyle;
+  whileTapBg?: ViewStyle;
 }
 
 export const Clickable: React.FC<Props> = ({
   children,
   viewStyle,
+  initialBg,
+  whileTapBg,
   ...props
 }) => (
   <Motion.Pressable {...props}>
     <Motion.View
       style={viewStyle}
-      initial={bg.white}
-      whileTap={bg.gray50}
+      initial={initialBg || bg.white}
+      whileTap={whileTapBg || bg.gray50}
       {...springMotion}
     >
       <Motion.View whileTap={{ scale: 0.95 }} {...springMotion}>
